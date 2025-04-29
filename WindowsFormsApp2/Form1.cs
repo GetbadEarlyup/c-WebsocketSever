e0ab149e49bddb39fcef3a4d0bc01eec70014363
GetbadEarlyup <42108721+GetbadEarlyup@users.noreply.github.com>
2025-04-29 17:24:38 +0800

    Add files via upload
    这是一个winFrom的小工具，用于再本机创建一个c#服务的项目。
    
    1、将本机ip地址改为左上角Ip，注意没有“：”后的部分，那是端口号。
    
    2、点击中间按钮，启动服务器
    
    3、如果启动成功，会在下面显示启动成功！
    
    
    
    4、如果有程序成功给该程序发了消息，则会在下面再显示一行文字
    
    显示包含：接收时间的时间戳，接收到对方的IP地址
    
    该系统采用多线程处理方式，性能较好
    
    5、打开写入文件：点击按钮打开一个文本文件，上面会记录传入的数据信息。
    
    6、清空文件数据信息：清空文件数据
    
    提示:并不建议在unity或其他系统中嵌套使用，一个是电脑性能影响较大，一个是线程和网络端口干扰问题

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fleck;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var server = new WebSocketServer("ws://192.168.110.2:8181");  //ws://localhost:8081    ws://127.0.0.0:8181  ws://192.168.0.146:8081
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Debug.WriteLine($"有新用户连入：{socket.ConnectionInfo.ClientIpAddress}");
+                };
+                socket.OnClose = () =>
+                {
+                    Debug.WriteLine($"用户断开连接：{socket.ConnectionInfo.ClientIpAddress}");
+                };
+                socket.OnMessage = message =>
+                {
+                    socket.Send($"服务器收到消息 : {DateTime.Now.ToString()}");
+                    Debug.WriteLine($"收到一条消息，来自：{socket.ConnectionInfo.ClientIpAddress}");
+                    if (label2.InvokeRequired)
+                    {
+                        Action SetText111 = delegate { SetText($"服务器收到消息 : {DateTime.Now.ToString()}" + $"收到一条消息，来自：{socket.ConnectionInfo.ClientIpAddress}"); };
+                        label2.Invoke(SetText111);
+                    }
+                    else
+                    {
+                        label2.Text = $"服务器收到消息 : {DateTime.Now.ToString()}" + $"收到一条消息，来自：{socket.ConnectionInfo.ClientIpAddress}";
+                    }
+
+                    Writr_Folider(message);
+                };
+            });
+
+            Debug.WriteLine("服务器已经启动！");
+            label1.Text = "服务器已经启动！";
+            
+        }
+        public void SetText(string SText)
+        {
+            label2.Text = SText;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1_Click(sender,e);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Writr_Folider(string str)
+        {
+            //if (!File.Exists("log.txt"))
+            //{
+            //    FileStream fs = new FileStream("log.txt", FileMode.Create, FileAccess.Write);
+            //}
+             File.AppendAllText("log.txt", "\r\n" + str);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("log.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+          DialogResult dr = MessageBox.Show("是否清空已写入的文件数据信息？", "操作警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            if (dr== DialogResult.OK)
+            {
+                System.IO.File.WriteAllText("log.txt", string.Empty);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
